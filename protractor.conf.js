8bb6880b95d5744167d7d0ff9abcf0fc3310b8f9
higorlaf12 <higor.freitas@neppoadm.com.br>
2017-12-08 15:32:43 -0200

    Todos os arquvios jasmine e allure, allure dando erro não cria pom.xml

---
+++ protractor.conf.js
@@ -0,0 +1,65 @@
+var SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+var AllureReporter = require('jasmine-allure-reporter');
+exports.config = {
+  // The address of a running selenium server.
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+
+  // Spec patterns are relative to the location of this config.
+  specs: [
+    'spec/*_spec.js'
+  ],
+
+
+  capabilities: {
+    'browserName': 'chrome',
+    'chromeOptions': {'args': ['--disable-extensions']}
+  },
+
+
+  // A base URL for your application under test. Calls to protractor.get()
+  // with relative paths will be prepended with this.
+
+    onPrepare:function() {
+
+        global.isAngularSite = function (flag) {
+            browser.ignoreSynchronization = !flag;
+        };
+
+        jasmine.getEnv().addReporter(new SpecReporter({
+            displayFailuresSummary: true,
+            displayFailuredSpec: true,
+            displaySuiteNumber: true,
+            displaySpecDuration: true
+        }));
+        jasmine.getEnv().addReporter(new AllureReporter({
+            resultsDir: 'allure-results'
+        }));
+        jasmine.getEnv().afterEach(function(done){
+            browser.takeScreenshot().then(function (png) {
+                allure.createAttachment('Screenshot', function () {
+                    return new Buffer(png, 'base64')
+                }, 'image/png')();
+                done();
+            })
+        });
+        beforeEach(function() {
+            originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 500000;
+        });
+        afterEach(function() {
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
+        });
+    },
+
+    allScriptsTimeout: 120000,
+    getPageTimeout: 120000,
+
+    jasmineNodeOpts: {
+        onComplete: null,
+        isVerbose: false,
+        showColors: true,
+        includeStackTrace: true,
+        defaultTimeoutInterval: 120000
+    }
+
+};
